c29b71eae7dd7630c9547f093302f269c1f398e4
nozomi107607 <natukihiko@gmail.com>
2022-06-30 04:56:55 +0000

    RANDOM_method_fix

--- app/controllers/public/films_controller.rb
+++ app/controllers/public/films_controller.rb
@@ -12,7 +12,7 @@ class Public::FilmsController < ApplicationController
   end
 
   def index
-    @random = Film.order("RAND()").all
+    @films = Film.order("RAND()").limit(10)
     @user = User.find(current_user.id)
   end
 
